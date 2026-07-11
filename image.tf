325c0b623ffb2d70a94dfee24bd4ebe247a32c66
dyra4eck <surefeur@yandex.ru>
2026-07-11 23:52:16 +0300

    add_resource: debian-cloude

---
+++ image.tf
@@ -0,0 +1,6 @@
+resource "proxmox_virtual_environment_download_file" "debian-cloud" {
+	content_type = "import"
+	datastore_id = "local"
+	node_name = var.node_name
+	url = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "node_name" {
+	type = string
+	description = "Имя ноды"
+}
+
+variable "pve_token" {
+	type = string
+	description = "Токен PVE-ноды"
+}
