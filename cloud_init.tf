17e31e00cb8184e5425affca78d558bd25e5f202
Тимоха <surefeur@yandex.ru>
2026-07-13 18:10:40 +0300

    change: vms + cloud_init

--- cloud_init.tf
+++ cloud_init.tf
@@ -1,14 +1,15 @@
 resource "proxmox_virtual_environment_file" "cloud_init" {
+	for_each = var.vms
 	content_type = "snippets"
 	datastore_id = "local"
 	node_name = var.node_name
 
 	source_raw {
 		data = templatefile("${path.module}/cloud-init/user-data.yaml.tftpl", {
-			vm_name = var.vm_name
+			vm_name = each.key
 			ssh_key = trimspace(file("~/.ssh/id_ed25519.pub"))
 			password_hash = var.password_hash
 		})
-		file_name = "user-data.yaml"
+		file_name = "user-data-${each.key}..yaml"
 	}
 }
